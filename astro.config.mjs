a35abff94f179f82fc9462862c692796cf98ba9d
Elayade Ismail <elayadeismail@gmail.com>
2024-07-19 17:39:28 +0100

    Setup tailwind and layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: true,
+		imageService: true,
+	}),
+});
