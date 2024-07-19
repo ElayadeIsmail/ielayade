803985539a06c25dfd2005e0c4e12ae323753390
Elayade Ismail <elayadeismail@gmail.com>
2024-07-19 17:39:56 +0100

    Add Sections

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as About } from './About.astro';
+export { default as Experience } from './Experience.astro';
+export { default as Footer } from './Footer.astro';
+export { default as Header } from './Header.astro';
+export { default as Projects } from './Projects.astro';
+export { default as ThemeIcon } from './ThemeIcon.astro';
