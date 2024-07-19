1a16c579093dc524475d89b3f225dc1d3584d0c3
Elayade Ismail <elayadeismail@gmail.com>
2024-07-19 23:01:55 +0100

    add menu

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const navItems = {
+	'#home': {
+		name: 'Home',
+		options: {},
+	},
+	'#experience': {
+		name: 'Experience',
+		options: {},
+	},
+	'#projects': {
+		name: 'Projects',
+		options: {},
+	},
+	'/ELAYADE_ISMAIL.pdf': {
+		name: 'Resume',
+		options: {
+			target: '_blank',
+			download: true,
+			rel: 'noopener noreferrer',
+		},
+	},
+};
