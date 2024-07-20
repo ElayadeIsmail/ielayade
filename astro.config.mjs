51a6eee33a1aef9921566ef9ea0074d69bba7c57
Elayade Ismail <elayadeismail@gmail.com>
2024-07-20 14:11:47 +0100

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/static';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ielayade.dev',
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
 		}),
+		sitemap(),
 	],
 	output: 'static',
 	adapter: vercel({
