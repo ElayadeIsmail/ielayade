8c4ff547012bcf32a82529c8c43aa3da400cdf15
Elayade Ismail <elayadeismail@gmail.com>
2024-07-21 18:18:30 +0100

    Fix resume downloaded name

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,22 @@
 export const navItems = {
-	'#home': {
-		name: 'Home',
-		options: {},
-	},
-	'#experience': {
-		name: 'Experience',
-		options: {},
-	},
-	'#projects': {
-		name: 'Projects',
-		options: {},
-	},
-	'/ELAYADE_ISMAIL.pdf': {
-		name: 'Resume',
-		options: {
-			target: '_blank',
-			download: true,
-			rel: 'noopener noreferrer',
-		},
-	},
+  "/#home": {
+    name: "Home",
+    options: {},
+  },
+  "/#experience": {
+    name: "Experience",
+    options: {},
+  },
+  "/#projects": {
+    name: "Projects",
+    options: {},
+  },
+  "/ELAYADE_ISMAIL.pdf": {
+    name: "Resume",
+    options: {
+      target: "_blank",
+      download: "ELAYADE_ISMAIL.pdf",
+      rel: "noopener noreferrer",
+    },
+  },
 };
